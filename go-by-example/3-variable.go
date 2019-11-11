8936b9e8e304680dedce534f2caedc15c970acfe
Dinesh Katwal <medineshkatwal@gmail.com>
2019-11-11 21:49:02 +0530

    variable example added

---
+++ go-by-example/3-variable.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+	var b, c int = 1, 2 // Initializing in single line
+	fmt.Println(b, c)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "apple"
+	fmt.Println(f)
+}
