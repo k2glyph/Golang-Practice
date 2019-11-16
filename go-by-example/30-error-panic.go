ec887f6615e307c9956a7d733f44ba859ff923f0
dinesh katwal <dinesh@auzmor.com>
2019-11-16 12:17:02 +0530

    Added example of error-panic

---
+++ go-by-example/30-error-panic.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+type customError struct {
+	arg     string
+	problem string
+}
+
+func (err *customError) Error() string {
+	return fmt.Sprintf("%s: %s", err.arg, err.problem)
+}
+func f1(x int) (int, error) {
+	if x == 42 {
+		return -1, &customError{arg: "integer", problem: "Can't do anything because number is 42"}
+	}
+	return x, nil
+}
+func main() {
+	fmt.Println("Panic Error example")
+	for i := 0; i < 50; i = i + 2 {
+		if val, e := f1(i); e == nil {
+			fmt.Println("f1 works", val)
+		} else {
+			// fmt.Println(e.Error())
+			panic(e.Error())
+		}
+	}
+
+}
